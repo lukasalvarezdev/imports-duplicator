0af3c64ac0b8d7ef0126ea34f3610d04a624dab0
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2022-05-28 01:04:41 -0500

    ref: remove big file

--- test/dir1/dir2/dir3/index.ts
+++ test/dir1/dir2/dir3/index.ts
@@ -5,7 +5,5 @@ import {
   MultiLine2,
 } from "../../../src/multi-line";
 import { WithMultipleDots } from "../../../src/with.multiple.dots";
-import { BigFile } from "../../../src/big.file";
 
 export type { Basic, DoubleQuotes, MultiLine, MultiLine2, WithMultipleDots };
-export { BigFile };
